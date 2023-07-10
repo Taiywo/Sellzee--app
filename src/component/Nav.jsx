471dd505d14e371fdc92e390fac3e2b9c20bdf5c
Taiywo <54299107+Taiywo@users.noreply.github.com>
2023-07-10 07:56:59 +0100

    Update Nav.jsx

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -4,12 +4,12 @@ import Logo from "../Asset/logo.png"
 function Nav(Navlink) {
   const {home, about, contact, testimonial, pricing} = Navlink
   return (
-    <div className='flex justify-around  items-center pt-[50px] '>
+    <div className='flex  justify-evenly -ml-60 items-center py-[50px]'>
       <img src="" alt="" srcset="" />
         <div>
            <img src={Logo} alt=''/>
         </div>
-        <ul className='flex gap-[29px] text-white text-[20px] font-medium leading-loose font-Inter'>
+        <ul className='flex gap-[50px] text-white text-[20px] font-medium leading-loose font-Inter'>
             <li><a href={home}>Home</a></li>
             <li><a href={about}>About</a></li>
             <li><a href={contact}>Contact</a></li>
